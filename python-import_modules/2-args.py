7de7ea66d4bd5df4fb0b2005fb0161c7fd68525d
root <root@ip-10-42-109-50.ec2.internal>
2026-01-16 10:36:24 +0000

    P3 T2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    i = len(argv) - 1
+    if i == 0:
+        print("{0} arguments.".format(i))
+    elif i == 1:
+        print("{0} argument:".format(i))
+    else:
+        print ("{i} arguments:".format(i=i))
+    for pos in range(1, i + 1):
+        print("{}: {}".format(pos, argv[pos]))
