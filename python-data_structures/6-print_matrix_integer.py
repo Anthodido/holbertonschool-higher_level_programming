e5609919070116e5de430f93179aa5e9ed7ae917
root <root@ip-10-42-162-91.ec2.internal>
2026-01-19 17:06:22 +0000

    P0 T6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for y in range(len(i)):
+            if y == len(i) - 1:
+                end= ""
+            else:
+                end= " "
+            print("{:d}".format(i[y]), end=end)
+        print()
