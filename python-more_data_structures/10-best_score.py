0e6f0490056ccc77fe9b99abe233a03c337fd6c6
root <root@ip-10-42-214-172.ec2.internal>
2026-01-21 14:33:26 +0000

    P1 T10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    m = sorted(a_dictionary, key=a_dictionary, reverse=True)
+    m = sorted(a_dictionary, key=a_dictionary.get, reverse=True)
     return m 
