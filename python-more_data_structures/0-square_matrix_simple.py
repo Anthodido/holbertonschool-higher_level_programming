67444e1ae359c12e63d301e2e9abbb657484d871
root <root@ip-10-42-214-172.ec2.internal>
2026-01-21 09:09:37 +0000

    P1 T0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new = []
+    for i in matrix:
+        new_r = []
+        for y in i:
+            new_v = y * y
+            new_r.append(new_v)
+        new.append(new_r)
+    return new
