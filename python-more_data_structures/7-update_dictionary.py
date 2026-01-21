da07cd6cd5c8bd2fd83d575f5776d30a667a8373
root <root@ip-10-42-214-172.ec2.internal>
2026-01-21 13:07:22 +0000

    P1 T7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value  """ca remplace si clé existe et créer si non"""
+    a_dictionary[key] = value
     return a_dictionary
