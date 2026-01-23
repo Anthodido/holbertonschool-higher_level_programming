874dee29e03427f204c1c4d359a06bc3cd378d5f
root <root@ip-10-42-51-211.ec2.internal>
2026-01-23 09:22:08 +0000

    P3 T0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,8 +12,8 @@ def add_integer(a, b=98):
 Floats are cast to ints.
 Raises TypeError on bad types.
 """
-    if type (a) not in (int, float):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type (b) not in (int, float):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
