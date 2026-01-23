2d6c2b2da2ae6c180947ed45b34213f63fd8d120
root <root@ip-10-42-51-211.ec2.internal>
2026-01-23 09:14:43 +0000

    P3 T0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add integer module.
+Provides add_integer function.
+Validates types.
+Casts floats to ints.
+Returns integer sum.
+"""
+
+
+def add_integer(a, b=98):
+    """Add two numbers as integers.
+Floats are cast to ints.
+Raises TypeError on bad types.
+"""
+    if type (a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type (b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
