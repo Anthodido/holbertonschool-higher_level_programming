45ba2d4b77dc4f2da03e2e1e23bdb1175ad209ea
root <root@ip-10-42-190-56.ec2.internal>
2026-01-28 09:26:57 +0000

    P0 T1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Ce module définit une classe Square."""
+
+
+class Square:
+    """Initialise une nouvelle instance de Square.
+    size: taille du carré."""
+    def __init__(self, size):
+        self.__size = size
