52c2e9aacee7d3ff5f368d983aa792e385b917fa
root <root@ip-10-42-135-17.ec2.internal>
2026-01-29 10:29:11 +0000

    P1 T1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Ce module définit une classe Rectangle."""
+
+
+class Square:
+    """Initialise une nouvelle instance de Rectangle.
+    size: taille du rectangle."""
+    def __init__(self, size):
+        self.__size = size
