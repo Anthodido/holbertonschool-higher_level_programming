7db90ec20b56f9c09df315e8c17c1835284f89e2
root <root@ip-10-42-76-222.ec2.internal>
2026-02-03 13:46:27 +0000

    P0 T6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module qui définit une classe vide BaseGeometry."""
+
+
+class BaseGeometry:
+    def area(self):
+        """Lève une exception car l'aire n'est pas implémentée."""
+        raise Exception("area() is not implemented")
