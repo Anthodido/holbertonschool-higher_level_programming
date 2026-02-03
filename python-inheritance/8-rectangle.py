bf538791e2dd0c0b09bd987509d35cae5836bf64
root <root@ip-10-42-76-222.ec2.internal>
2026-02-03 14:21:42 +0000

    P0 T8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module qui définit une classe vide BaseGeometry."""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Module qui définit la classe Rectangle."""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
