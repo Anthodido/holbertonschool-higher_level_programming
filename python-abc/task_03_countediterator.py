0901bda148e039d056e64167fbae86eda65a95df
root <root@ip-10-42-213-36.ec2.internal>
2026-02-05 14:52:00 +0000

    P1 T3

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+        except StopIteration:
+            raise
+        self.count += 1
+        return item
+    def get_count(self):
+        return self.count
