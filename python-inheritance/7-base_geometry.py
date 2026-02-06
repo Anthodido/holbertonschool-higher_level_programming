b5438bddcea51559f16a253a170d9835e76c4bc6
Anthodido <anthony.didomenico678@gmail.com>
2026-02-06 14:18:27 +0000

    P0 T7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""Module qui définit une classe vide BaseGeometry."""
+"""Module with class BaseGeometry"""
 
 
 class BaseGeometry:
+    """BaseGeometry class"""
+
     def area(self):
-        """Lève une exception car l'aire n'est pas implémentée."""
+        """Method for calculated area"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Valide que value est un entier strictement positif."""
+        """Method for integer validation"""
+
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
