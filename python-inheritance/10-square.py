2944f9167b1ae0ad550b6b27299e042b3112e427
Anthodido <anthony.didomenico678@gmail.com>
2026-02-06 14:21:03 +0000

    P0 T10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Module qui définit une classe vide BaseGeometry."""
+"""Defines a Square class"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
     def __init__(self, size):
-        super().integer_validator("size", size)
+        """Initialize a Square"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square"""
+        return self._Rectangle__width ** 2
