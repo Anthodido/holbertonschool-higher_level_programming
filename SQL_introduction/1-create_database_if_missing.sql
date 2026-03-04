29028f3102ac31e0ef9160847f7f3bd7aacdec73
root <root@ip-10-42-126-153.ec2.internal>
2026-03-04 21:51:17 +0000

    reprise de tout les exo

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+--creates the database hbtn_0c_0
+--create database hbtn_0c_0 if missing
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+--deletes the database hbtn_0c_0
+--not fail if hbtn_0c_0 is missing
+DROP DATABASE IF EXISTS hbtn_0c_0;
