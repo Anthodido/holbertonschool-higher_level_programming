756b492149f93f4097e2823d289c0676e72d7826
Anthodido <anthony.didomenico678@gmail.com>
2026-03-20 19:35:27 +0100

    .

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,8 @@
+#!/usr/bin/node
+/* 1-multi_languages.js
+Write a JavaScript script that prints "JavaScript is amazing" in multiple languages
+*/
+
 const first = "C is fun"
 const second = "Python is cool"
 const third = "JavaScript is amazing"
