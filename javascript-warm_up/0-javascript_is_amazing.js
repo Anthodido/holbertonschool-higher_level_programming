54bd5d27c61ead3e62cb72f7dccfbe15f11f44f6
Anthodido <anthony.didomenico678@gmail.com>
2026-07-01 10:25:05 +0200

    erreur

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -5,5 +5,5 @@ Write a JavaScript script that prints "JavaScript is amazing"
 */
 
 
-const myVar = "JavaScript is amazing"
-console.log(myVar)
+const myVar = "JavaScript is amazing";
+console.log(myVar);
