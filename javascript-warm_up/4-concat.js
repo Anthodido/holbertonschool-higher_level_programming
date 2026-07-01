3759e22a3649f7ec289d7d354cadab74502dd533
Anthodido <anthony.didomenico678@gmail.com>
2026-07-01 10:49:56 +0200

    style

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* 4-concat.js
-Write a script that prints two arguments passed to it, in the following format: " is "
+Write a script that prints two arguments passed to it,
+in the following format: " is "
 */
 
 if (process.argv[2] && process.argv[3]) {
