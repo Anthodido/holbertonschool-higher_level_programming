962a792ceeda7b75540b092dd5edab2ac8e16486
Anthodido <anthony.didomenico678@gmail.com>
2026-07-02 10:03:00 +0200

    style

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -7,6 +7,7 @@ is amazing" in multiple languages
 const first = "C is fun"
 const second = "Python is cool"
 const third = "JavaScript is amazing"
+
 console.log(first)
 console.log(second)
 console.log(third)
